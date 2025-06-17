5dc1118c9818a6a3e2b0ce79f2cbc6bcbbac31bf
Yi Miao <yimiaoheidelberg@gmail.com>
2025-06-17 21:35:37 +0200

    Initialer Commit: git_Tutorial automatisiert

---
+++ README.md
@@ -0,0 +1,5 @@
+# In diesem Projket werden Grundlagen für Git dargestellt.
+# Autor: Yi Miao
+# Ort: Heidelberg
+# Datum: 17-06-2025
+# Bis: 
---
+++ grundlagen_Git.ps1
@@ -0,0 +1,9 @@
+# 1
+# Eine Repository auf Github per Powershell anlegen
+# einen Ordner für das Projekt anlegen
+cd ~
+mkdir git_Tutorial
+cd .\git_Tutorial\
+
+# Jetzt überprüfe ich, ob ich schon git installiert habe
+git -v
